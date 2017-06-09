1ccae86c1e3e6ca1139901e2a12efed5b1376de2
Judi-C <judith.caroff@isen.yncrea.fr>
2017-06-09 13:24:28 +0200

    ajout de l'application super belle d'une super belle personne extra géniale

---
+++ ApplicationBoiteAuxLettres/ApplicationBoiteAuxLettres/src/pages/AjoutCompte/AjoutCompte.ts
@@ -0,0 +1,38 @@
+﻿import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-AjoutCompte',
+    templateUrl: 'AjoutCompte.html'
+})
+export class AjoutComptePage {
+
+    constructor(private toastCtrl: ToastController) { }
+
+    /*todo = {}
+    logForm() {
+        console.log(this.todo)
+    }*/
+
+
+    presentToast() {
+        
+        let toast = this.toastCtrl.create({
+            message: 'Le compte a bien été créé',
+            duration: 3000,
+            position: 'top'
+        });
+
+        toast.onDidDismiss(() => {
+            console.log('Dismissed toast');
+        });
+
+        toast.present();
+    }
+
+    /*ionViewDidLoad() {
+        console.log('ionViewDidLoad AjoutComptePage');
+    }*/
+
+}
---
+++ ApplicationBoiteAuxLettres/ApplicationBoiteAuxLettres/src/pages/about/about.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { AjoutComptePage } from '../AjoutCompte/AjoutCompte';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {
+
+    }
+
+  goToOtherPage() {
+      this.navCtrl.push(AjoutComptePage);
+  }
+
+}

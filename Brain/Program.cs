e595eb0a7a54b20da5c54647dc1a41e501962654
Luc Fermaut <luc.fermaut@hotmail.fr>
2017-06-14 17:27:18 +0200

    mise a jour du brain avec plein de fonctionnalités

--- Brain/Program.cs
+++ Brain/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace brain2._0
 {
@@ -23,7 +25,7 @@ namespace brain2._0
 
         private int nextTime = -1;
 
-        Boolean DoorOpened = false;
+        Boolean ActivateMotor = false;
 
 
 
@@ -37,20 +39,69 @@ namespace brain2._0
             PackageHost.WriteInfo("Package starting - IsRunning: {0} - IsConnected: {1}", PackageHost.IsRunning, PackageHost.IsConnected);
             Users = new List<User>();
             Notifs = new List<Notif>();
+            this.loadUsers();
             this.Distance.ValueChanged += Distance_ValueChanged;
             this.UID.ValueChanged += UID_ValueChanged;
-            Users.Add(new User("ad795bb", false, "monsieur", "facteur"));
-            Users.Add(new User("4a96d8b", true, "pierre", "hourde"));
-            PackageHost.PushStateObject("Users",Users);
+            initializeStateObject();
+
             while (PackageHost.IsRunning)
             {
 
             }
         }
 
+        public override void OnPreShutdown()
+        {
+            saveUsers();
+        }
+
+        public void initializeStateObject()
+        {
+            PackageHost.PushStateObject("Users", Users);
+            PackageHost.PushStateObject("Notification", Notifs);
+            PackageHost.PushStateObject("ActivateMotor", ActivateMotor);
+            PackageHost.PushStateObject("newUser", "");
+        }
+
+        public void loadUsers()
+        {
+            string line;
+            StreamReader file = new StreamReader(@"C:\Luc\cours\CIR 3\Constellation\brain\brain\UsersSave.txt");
+            while((line = file.ReadLine())!= null)
+            {
+                string uid = line;
+                string client = file.ReadLine();
+                Boolean b;
+                string firstName = file.ReadLine();
+                string name = file.ReadLine();
+                if(client == "True")
+                {
+                    b = true;
+                }
+                else
+                {
+                    b = false;
+                }
+                Users.Add(new User(uid, b , firstName, name));
+            }
+        }
+        public void saveUsers() {
+            List<string> save = new List<string>();
+            StreamWriter file = new StreamWriter(@"C:\Luc\cours\CIR 3\Constellation\brain\brain\UsersSave.txt");
+
+            foreach (User user in Users)
+            {
+                file.WriteLine(user.uid);
+                file.WriteLine(user.client.ToString());
+                file.WriteLine(user.firstName);
+                file.WriteLine(user.name);
+            }
+
+            file.Close();
+        }
         private void Distance_ValueChanged(object sender, StateObjectChangedEventArgs e)
         {
-            if (!DoorOpened)
+            if (!ActivateMotor)
             {
                 if (!e.IsNew)
                 {
@@ -76,19 +127,19 @@ namespace brain2._0
             PackageHost.PushStateObject("Notification", Notifs);
         }
 
-        public void changeDoor()
+        public void changeMotor()
         {
-            if (DoorOpened)
+            if (ActivateMotor)
             {
-                DoorOpened = false;
+                ActivateMotor = false;
                 PackageHost.WriteInfo("porte fermée");
             }
             else
             {
-                DoorOpened = true;
+                ActivateMotor = true;
                 PackageHost.WriteInfo("porte ouverte");
             }
-            PackageHost.PushStateObject("doorOpened", DoorOpened);
+            PackageHost.PushStateObject("ActivateMotor", ActivateMotor);
         }
 
         public void UID_ValueChanged(object sender, StateObjectChangedEventArgs e){
@@ -96,8 +147,8 @@ namespace brain2._0
             PackageHost.WriteInfo(e.NewState.DynamicValue);
             int id = findUser(e.NewState.DynamicValue);
             if (id != -1) {
-                changeDoor();
-                if (DoorOpened) {
+                changeMotor();
+                if (ActivateMotor) {
                     if (this.Users[id].client) {
                         Notifs = new List<Notif>();
                         PackageHost.WriteInfo($"{this.Users[id].firstName} {this.Users[id].name} a récupéré le courrier");
@@ -121,21 +172,34 @@ namespace brain2._0
         [MessageCallback]
         public void AddUser(string uid, Boolean type, string firstName, string name)
         {
-            PackageHost.WriteInfo($"new user :{uid}");
-
-            if (uid == "")
-            {
-                PackageHost.PurgeStateObjects("newUser");
-                PackageHost.WriteInfo("ajout carte abandonné");
-            }
-            else
-            {
                 Users.Add(new User(uid, type, firstName, name));
                 PackageHost.WriteInfo("ajout carte validé");
+                PackageHost.PushStateObject("Users", Users);
+            saveUsers();
+        }
+
+        [MessageCallback]
+        public void DeleteUser(string uid, Boolean type, string firstName, string name)
+        {
+            int id = FindUser(uid, type, firstName, name);
+            if(id!=-1){
+                Users.Remove(Users[id]);
+                PackageHost.WriteInfo($"le compte de {firstName} {name} a été supprimé");
 
                 PackageHost.PushStateObject("Users", Users);
+                saveUsers();
             }
+        }
 
+        public int FindUser(string uid, Boolean type, string firstName, string name) {
+            for (int i = 0; i < this.Users.Count; i++)
+            {
+                if ((Users[i].name == name) && (Users[i].uid == uid) && (Users[i].client == type) && (Users[i].firstName == firstName))
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         public int findUser(string uid)
--- Brain/README.md
+++ Brain/README.md
@@ -6,12 +6,20 @@ pour récupérer le package :
 
 - dans le dossier de visual studio supprimer le fichier 'programe.cs'
 
-- copier coller les trois fichiers 'User.cs', 'Notif.cs' et 'programme.cs' au même endroit
+- copier coller les quatre fichiers 'User.cs', 'Notif.cs', 'programme.cs' et 'UsersSave.txt'au même endroit
 
 - dans visual studio -> click droit sur brain 
 			-> ajouter 
 			-> element existants 
-			-> selectionner les trois fichiers ajouter précedement 
+			-> selectionner les quatre fichiers ajouter précedement 
+
+ATTENTION CHEMIN COMPLET 
+dans le code il faut changer dans les fonctions loadUsers et saveUsers le chemin du fichier 'UsersSave.txt' 
+et mettre votre propre chemin, pour l'instant le chemin relatif ne fonctionne pas.
+
+pour trouver le chemin, click droit sur 'UsersSave.txt' -> propriété -> chemin complet -> copier/coller et ca part
+
+Si vous faites pas ca ca ne fonctionnera pas !! on vient pas se plaindre les lapins, vous etes prevenus <3
 
 sur visual studio :
 - tester le package en mode debug en appuyant sur la fleche noir dans un cercle vert
